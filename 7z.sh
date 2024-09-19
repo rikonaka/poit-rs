9824464c25150b77a731082210954a35c6e3b9b5
RikoNaka <xxy1836@gmail.com>
2024-09-19 17:21:07 +0800

    Add 7z script

---
+++ 7z.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+
+rm poit-x86_64-unknown-linux-musl.7z
+rm poit-aarch64-unknown-linux-musl.7z
+rm poit-i586-unknown-linux-musl.7z
+
+7z a -mx9 poit-x86_64-unknown-linux-musl.7z ./target/x86_64-unknown-linux-musl/release/poit
+7z a -mx9 poit-aarch64-unknown-linux-musl.7z ./target/aarch64-unknown-linux-musl/release/poit
+7z a -mx9 poit-i586-unknown-linux-musl.7z ./target/i586-unknown-linux-musl/release/poit
